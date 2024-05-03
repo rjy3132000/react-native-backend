fee04f723fc0f762cd0f779389878faddcea4316
Rajesh <ry292000@gmail.com>
2024-05-03 13:30:52 +0530

    added the cors issue

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-const express = require('express');
-const connectDb = require('./config/db');
+const express = require("express");
+const connectDb = require("./config/db");
 const routes = require("./routes/index");
+const cors = require("cors");
 const app = express();
 
-connectDb();
-
+app.use(cors());
 app.use(express.json());
+connectDb();
 app.use(routes);
 
 const PORT = 5000;
-app.listen(PORT, () => {console.log(`Server Started on PORT:= ${PORT}`);});
+app.listen(PORT, () => {
+  console.log(`Server Started on PORT:= ${PORT}`);
+});
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const { authUserLogin } = require("../controller/Login");
-const { createNote, updateNote, delteNote, getNoteById } = require("../controller/Notes");
+const {
+  createNote,
+  updateNote,
+  delteNote,
+  getNoteById,
+} = require("../controller/Notes");
 const { protect } = require("../middleware/authMiddleware");
 const router = express.Router();
 
-router.route('/').get((req, res) => { res.send('API is working') });
-router.route('/login').post(authUserLogin);
-router.route("/add-note").post(protect,createNote)
-router.route("/update-note/:id").post(protect,updateNote);
-router.route("/delete-note/:id").delete(protect,delteNote);
-router.route("/note/:id").delete(protect,getNoteById);
+router.route("/").get((req, res) => {
+  res.json({ message: "API is working" });
+});
+router.route("/login").post(authUserLogin);
+router.route("/add-note").post(protect, createNote);
+router.route("/update-note/:id").post(protect, updateNote);
+router.route("/delete-note/:id").delete(protect, delteNote);
+router.route("/note/:id").delete(protect, getNoteById);
 
 module.exports = router;
